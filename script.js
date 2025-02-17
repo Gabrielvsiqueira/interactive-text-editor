3309f1852c800df3ae83098e4ad83a7e97ef8794
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-02-16 22:57:09 -0300

    projeto final

--- script.js
+++ script.js
@@ -18,3 +18,46 @@ function baixarArquivo(type) {
     link.download = filename + "." + type;
     link.click();
 }
+
+function alterarEstilo(estilo) {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+
+    if (!selection.rangeCount) return;
+
+    const span = document.createElement('span');
+    span.style.fontWeight = estilo === 'bold' ? 'bold' : '';
+    span.style.fontStyle = estilo === 'italic' ? 'italic' : '';
+    span.style.textDecoration = estilo === 'underline' ? 'underline' : '';
+    
+    range.surroundContents(span);
+}
+function alterarFonte(fonte) {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const selectedText = selection.toString();  
+    if (selectedText) {
+        let element = document.createElement('span');
+        element.style.fontFamily = fonte; 
+
+        element.appendChild(range.extractContents());
+        range.insertNode(element);
+    } else {
+        alert('Selecione algum texto primeiro!');
+    }
+}
+
+function alterarCor(cor) {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const selectedText = selection.toString();  
+    if (selectedText) {
+        let element = document.createElement('span');
+        element.style.color = cor; 
+
+        element.appendChild(range.extractContents());
+        range.insertNode(element);
+    } else {
+        alert('Selecione algum texto primeiro!');
+    }
+}
